2ccbe372b4ff0586a3857439104dcdfd4315a6eb
Akash Basu <akashbasu1992@gmail.com>
2017-03-29 20:23:00 +0530

    Basic Scene Layout

---
+++ Assets/Scripts/Game/MainSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MainSceneManager : MonoBehaviour {
+
+    private GameObject paddlePrefab;
+	private GameObject brickPrefab;
+
+
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+        string temp = SceneManagerConstants.PrefabPaths.prefabRoot;
+        //paddlePrefab = AssetDatabase.LoadAssetAtPath<GameObject>("");
+    }
+    // Use this for initialization
+    void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/Game/SceneManagerConstants.cs
@@ -0,0 +1,9 @@
+static class SceneManagerConstants
+{
+    public class PrefabPaths
+    {
+        public const string prefabRoot = "/Prefabs/";
+        public const string paddle = "Paddle";
+    }
+
+}
